1384a7d2841d24ab3b86d3d225faa27910132917
Jacob Greenway <jagreenway12@gmail.com>
2017-10-21 13:57:00 -0500

    added fragments for separate menus

--- app/src/main/java/hacktober/snoozify/MainActivity.java
+++ app/src/main/java/hacktober/snoozify/MainActivity.java
@@ -3,13 +3,16 @@ package hacktober.snoozify;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import hacktober.snoozify.fragments.AlarmsFragment;
+import hacktober.snoozify.fragments.HistoryFragment;
+import hacktober.snoozify.fragments.PaymentSettingsFragment;
 
-    private TextView mTextMessage;
+public class MainActivity extends FragmentActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +20,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_alarms);
+                case R.id.navigation_alarms:
+                    replaceFragment(new AlarmsFragment());
                     return true;
-                case R.id.navigation_dashboard:
-                    //mTextMessage.setText(R.string.title_payment_settings);
+                case R.id.navigation_payment_settings:
+                    replaceFragment(new PaymentSettingsFragment());
                     return true;
-                case R.id.navigation_notifications:
-                    //mTextMessage.setText(R.string.title_history);
+                case R.id.navigation_history:
+                    replaceFragment(new HistoryFragment());
                     return true;
             }
             return false;
@@ -37,9 +40,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            Fragment alarmsFragment = new AlarmsFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            alarmsFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, alarmsFragment).commit();
+        }
+
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    // Replace the current fragment with a new one passed in
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
     }
 
 }
